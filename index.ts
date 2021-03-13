aa33c53a2b350d3d0b36328855ddb6bfdd5f9abf
Nicholas Osei <Niconaldodonjunior@yahoo.com>
2021-03-13 22:33:28 +0100

    deploy action

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ export const handler:Handler = async (event:APIGatewayProxyEvent):Promise<APIGat
     const val = _.random(max);
     const response = {
         statusCode: 200,
-        body:'The random value (max ${max}) is: ${val}',
+        body:"The random value (max" +max+") is: "+ val+"}",
     };
     return response;
 }
